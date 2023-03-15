cee10e3d763b97003abfc833275292bd0c684763
Bruno Saldanha <bsaldanha1990@gmail.com>
2023-03-15 20:20:13 -0300

    criando a logica no javascript

---
+++ main.js
@@ -0,0 +1,63 @@
+let listaItem = [];
+
+const form = document.getElementById("form-itens");
+const itensInput = document.getElementById("receber-item");
+
+form.addEventListener("submit", function(evento){
+    evento.preventDefault();
+    salvarItem();
+})
+
+function salvarItem(){
+    const comprasItem = itensInput.value;
+    const checarDuplicado = listaItem.some((elemento) => elemento.valor === comprasItem);
+
+    if(checarDuplicado){
+        alert('item ja existe');
+    } else
+    listaItem.push({
+        valor: comprasItem;
+    })
+    console.log(listaItem); 
+}
+
+
+
+/*const listaDeItens = {
+    item1: 'bergamota',
+    item2: 'laranja',
+    item3: 'poca',
+    quant1: 2,
+    quant2: 3,
+    quant3: 4,
+
+
+    chamaItem: function(){
+        alert('o item comprado foi ' + listaDeItens.item1 + ' e sua quantidade foi '+ listaDeItens.quant1);
+    }
+}
+
+listaDeItens.chamaItem();
+
+const cliente = {
+    nome: 'Bruno',
+    idade: 33,
+
+
+    printaIdade: function() {
+
+        
+        for( idade = 33; idade <= 100; idade++){
+            console.log('sua idade é: '+ idade);
+        }
+    }
+}
+
+cliente.cpf = '02117133096';
+cliente.cidade = 'pernambuco';
+
+console.log('Meu nome é: ' + cliente.nome + ' e meu cpf é: ' + cliente.cpf + ' e moro na cidade de ' + cliente.cidade);
+
+
+cliente.printaIdade();*/
+
